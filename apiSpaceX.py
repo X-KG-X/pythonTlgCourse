b0888bf3e2296936674031b90f413f47e864497a
KG <xushalx@noreply.non>
2020-06-30 17:34:50 -0400

    Day 2 afternoon

--- apiSpaceX.py
+++ apiSpaceX.py
@@ -5,25 +5,20 @@ This program harvests SpaceX data avail from https://api.spacexdata.com/v3/cores
 """
 
 # using std library method for getting at API data
-import urllib.request 
-import json
+import requests 
 
 # GOBAL / CONSTANT of the API we want to lookup
 SPACEXURI = "https://api.spacexdata.com/v3/cores"
 
 def main():
     # create a urllib.request response object by sending an HTTP GET to SPACEXURI
-    coreData = urllib.request.urlopen(SPACEXURI)
+    coreData = requests.get(SPACEXURI)
 
     # pull STRING data off of the 200 response (even tho it's JSON!)
-    xString = coreData.read().decode()
-    print(type(xString))
+    listData = coreData.json()
+    print(type(listData))
 
-    # convert STRING data into Python Lists and Dictionaries
-    listOfCores = json.loads(xString)
-    print(type(listOfCores))
-
-    for core in listOfCores:
+    for core in listData:
         print(f"{core['core_serial']}'s original launch date was {core['original_launch']}", end="\n")
         print("Missions:")
         for mission in core['missions']:
