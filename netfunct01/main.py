d8d220652811140d156730ba4d5ce87a08279fff
KG <xushalx@noreply.non>
2020-06-30 22:26:33 -0400

    Lab 28 3rd customization, yaml intro

--- netfunct01/main.py
+++ netfunct01/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Alta3 Research || Author: RZFeeser@alta3.com"""
-
+import yaml
 # function to push commands
 def commandpush(devicecmd): # devicecmd==list
     for coffeetime in devicecmd.keys():
@@ -19,8 +19,8 @@ def devicereboot(ipList):
 
 # start our main script
 def main():
-    work2do = {"10.1.0.1":["interface eth1/2", "no shutdown"], "10.2.0.1": 
-    ["interface eth1/1", "shutdown"], "10.3.0.1":["interface eth1/5", "no shutdown"]} 
+    #work2do = {"10.1.0.1":["interface eth1/2", "no shutdown"], "10.2.0.1": 
+    #["interface eth1/1", "shutdown"], "10.3.0.1":["interface eth1/5", "no shutdown"]} 
     # data that replaces data stored in file
 
     print("Welcome to the network device command pusher") # welcome message
@@ -28,6 +28,9 @@ def main():
     ## get data set
     print("\nData set found\n") # replace with function call that reads in data from file
 
+    # get data from external yaml file
+    with open('work2do.yaml') as data:
+        work2do=yaml.load(data, Loader=yaml.FullLoader)
     ## run
     commandpush(work2do) # call function to push commands to devices
 
