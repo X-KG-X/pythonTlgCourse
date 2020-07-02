d2f2d275a31804d9cd07f02bc7eaa31215248fd9
KG <xushalx@noreply.non>
2020-07-01 22:22:08 -0400

    pypai labs init

---
+++ pyapi/iceAndFire04.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Alta3 Research - Exploring OpenAPIs with requests"""
+# documentation for this API is at
+# https://anapioficeandfire.com/Documentation
+
+import requests
+
+AOIF_CHAR = "https://www.anapioficeandfire.com/api/characters"
+
+def main():
+    ## Ask user for input
+    got_charToLookup = input("What is the name of the character we should lookup? " )
+
+    ## Send HTTPS GET to the API of ICE and Fire character resource
+    gotresp = requests.get(AOIF_CHAR + "?name=" + got_charToLookup)
+
+    ## Decode the response
+    got_dj = gotresp.json()
+
+    print(got_dj)
+    print(f"The character {got_charToLookup} has the URL: {got_dj[0]['url']}")
+    
+    if got_dj is not None:
+        if got_dj[0]['allegiances'] is not None:
+            house=requests.get(got_dj[0]['allegiances'][0])['name']
+            print(f"{got_charToLookup}'s allegiance is to {house}")
+
+if __name__ == "__main__":
+    main()
+
