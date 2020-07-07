a682c0de87e230f3588918b23365711b3140c98b
xushalx <xushalx@gmail.com>
2020-07-07 15:09:25 -0700

    ldap tests conn.search

--- ldap3Play/lTest.py
+++ ldap3Play/lTest.py
@@ -6,8 +6,16 @@ conn = Connection(server, 'uid=admin,cn=users,cn=accounts,dc=demo1,dc=freeipa,dc
 conn.search('dc=demo1,dc=freeipa,dc=org','(objectclass=person)', attributes=['sn', 'krbLastPwdChange', 'objectclass'])
 
 print("*********************************************")
-print(conn.entries)
-print("------------")
-print(server)
-print("------------")
-print(server.info)
+
+
+searchParameters = { 'search_base': 'dc=demo1,dc=freeipa,dc=org','search_filter': '(objectClass=Person)','attributes': ['cn', 'givenName'],'paged_size': 5 }
+
+while True:
+    conn.search(**searchParameters)
+    for entry in conn.entries:
+        print(entry)
+    cookie = conn.result['controls']['1.2.840.113556.1.4.319']['value']['cookie']
+    if cookie:
+        searchParameters['paged_cookie'] = cookie
+    else:
+        break
