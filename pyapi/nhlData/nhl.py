3b8612f09e752ab20cbb991026fe47bab725f443
xushalx <xushalx@gmail.com>
2020-07-07 10:08:04 -0700

    nhl pandas plt addition

--- pyapi/nhlData/nhl.py
+++ pyapi/nhlData/nhl.py
@@ -5,20 +5,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
+    #get data
     resp=requests.get("https://statsapi.web.nhl.com/api/v1/teams").json()
 
+    #Division list dict
+    divDict=[]
+    #print response data
     for one in resp['teams']:
         print(f"Team Name: {one['name']}, \t\t Stadium Name: {one['venue']['name']}")
-
+        divDict.append(one['division'])
+    #create pandas dataframe
     nhldf=pd.DataFrame(resp['teams'])
     print(nhldf.head())
 
-    # nhlLocation=nhldf.groupby("locationName").agg('count')
-    # print(nhlLocation)
-    nhldivDf=pd.DataFrame(nhldf['division'])
+    #create df for list of Division dictDict
+    nhldivDf=pd.DataFrame(divDict)
     print(nhldivDf.head())
-    print(type(nhldivDf))
+    #groupby division id
+    divGrp=nhldivDf.groupby('id').agg({'id':['count']})
+    print()
+    print(divGrp)
+    #plot the groupby division id
+    divGrp.plot(kind="barh")
+    plt.show()
     
-
 if __name__=="__main__":
     main()
